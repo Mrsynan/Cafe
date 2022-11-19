fec23b0802ff9c5b3c6b15a6ae27622f8235d1bd
Mrsynan <99237339+Mrsynan@users.noreply.github.com>
2022-11-19 13:40:10 +0700

    Add files via upload

---
+++ CafeShop/CafeShop/Login.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace CafeShop
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Swis721 BdOul BT", 27F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(50, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(481, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome My Coffee Shop";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(21, 320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(21, 410);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password :  ";
+            // 
+            // txtUser
+            // 
+            this.txtUser.BackColor = System.Drawing.Color.Sienna;
+            this.txtUser.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtUser.Location = new System.Drawing.Point(198, 317);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(333, 36);
+            this.txtUser.TabIndex = 3;
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.Color.Sienna;
+            this.txtPass.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtPass.Location = new System.Drawing.Point(198, 402);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(333, 36);
+            this.txtPass.TabIndex = 4;
+            this.txtPass.UseSystemPasswordChar = true;
+            // 
+            // btnLog
+            // 
+            this.btnLog.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnLog.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLog.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnLog.Location = new System.Drawing.Point(218, 510);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(135, 56);
+            this.btnLog.TabIndex = 5;
+            this.btnLog.Text = "Login";
+            this.btnLog.UseVisualStyleBackColor = false;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(561, 631);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Coffee Production";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnLog;
+    }
+}
+
---
+++ CafeShop/CafeShop/Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace CafeShop
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            if (txtUser.Text == " ")
+            {
+                MessageBox.Show("Enter Username : ");
+            }
+            else if(txtPass.Text == " ")
+            {
+                MessageBox.Show("Enter Password :";
+            }
+            else
+            {
+                try
+                {
+                    SqlConnection con = new SqlConnection();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("" + ex);
+                }
+            }
+        }
+    }
+}
